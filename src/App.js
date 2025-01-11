8d1c3be008ab86e3ba46c85002a37bc810ebbf81
Wojciech Frącz <fraczwojciech@gmail.com>
2025-01-11 12:05:19 +0100

    Reactivity

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let title = 'Wall-E';
+    const [title, setTitle] = useState('Wall-E');
+
+    let message;
+    if (title.length < 5) {
+        message = <div>Tutuł jest za krótki. Nagrywają takie filmy?</div>;
+    } else if (title.length < 15) {
+        message = <div>Tytuł jest ekstra, w sam raz na plakat przed kinem!</div>;
+    } else {
+        message = <div>Tytuł jest za długi, nikt tego nie zapamięta.</div>;
+    }
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setTitle(event.target.value);
     }
 
     return (
         <div>
             <h1>My favourite movies to watch</h1>
             <h2>My favourite movie for today is {title}</h2>
-            <input type="text" onChange={handleChange}/>
+            {title.length > 0 && <div>{message}</div>}
+            <input type="text" value={title} onChange={handleChange}/>
         </div>
     );
 }
