731b45e23c5b113747fc4bb2bead8c15dc963a36
Wojciech Frącz <fraczwojciech@gmail.com>
2025-01-11 12:37:10 +0100

    Movies list

--- src/App.js
+++ src/App.js
@@ -17,9 +17,20 @@ function App() {
         setTitle(event.target.value);
     }
 
+    const movies = [
+        {title: "Wall-E"},
+        {title: "Pulp Fiction"},
+        {title: "Matrix"},
+        {title: "1670"},
+    ];
+
     return (
         <div>
             <h1>My favourite movies to watch</h1>
+            <h2>Titles</h2>
+            <ul>
+                {movies.map(movie => <li key={movie.title}>{movie.title}</li>)}
+            </ul>
             <h2>My favourite movie for today is {title}</h2>
             {title.length > 0 && <div>{message}</div>}
             <input type="text" value={title} onChange={handleChange}/>
