c6114ecdd7480d4a6d8f294a7c3ba3ebb257e78f
Wojciech Frącz <fraczwojciech@gmail.com>
2025-01-11 14:00:32 +0100

    MovieForm

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
 import './App.css';
 import {useState} from "react";
 import "milligram";
+import MovieForm from "./MovieForm";
 
 function App() {
-    const [title, setTitle] = useState('');
-    const [year, setYear] = useState('');
     const [movies, setMovies] = useState([]);
-
-    let message;
-    if (title.length < 5) {
-        message = <div>Tutuł jest za krótki. Nagrywają takie filmy?</div>;
-    } else if (title.length < 15) {
-        message = <div>Tytuł jest ekstra, w sam raz na plakat przed kinem!</div>;
-    } else {
-        message = <div>Tytuł jest za długi, nikt tego nie zapamięta.</div>;
-    }
-
-    function addMovie(event) {
-        event.preventDefault();
-        if (title.length < 5) {
-            return alert('Tytuł jest za krótki');
-        }
-        setMovies([...movies, {title, year}]);
-        setTitle('');
-        setYear('');
-    }
 
     return (
         <div className="container">
@@ -33,19 +13,7 @@ function App() {
             <ul>
                 {movies.map(movie => <li key={movie.title}>{movie.title} ({movie.year})</li>)}
             </ul>
-            <h2>Add movie</h2>
-            <form onSubmit={addMovie}>
-                <div>
-                    <label>Tytuł</label>
-                    <input type="text" value={title} onChange={(event) => setTitle(event.target.value)}/>
-                    {title.length > 0 && <div>{message}</div>}
-                </div>
-                <div>
-                    <label>Rok nagrania</label>
-                    <input type="text" value={year} onChange={(event) => setYear(event.target.value)}/>
-                </div>
-                <button>Dodaj film</button>
-            </form>
+            <MovieForm onMovieSubmit={(movie) => setMovies([...movies, movie])} buttonLabel="Add a movie"/>
         </div>
     );
 }
