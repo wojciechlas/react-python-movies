faf8a7743a9ecfaa01e43a5589316eccaf6ff240
Wojciech Frącz <fraczwojciech@gmail.com>
2025-01-22 23:52:06 +0100

    Server static files from backend

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,15 @@
-FROM python:3.9
+FROM node:16.20.2-alpine AS frontend
+COPY ui/package.json /var/app/ui
+COPY ui/package-lock.json /var/app/ui
+WORKDIR /var/app/ui
+RUN npm ci
+COPY ui/src /var/app/ui
+RUN npm run build
 
-COPY requirements.txt /code/requirements.txt
-WORKDIR /code
-RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
-COPY . /code
+FROM python:3.9
+COPY --from=frontend /var/app/ui/build /var/app/ui/build
+COPY requirements.txt /var/app/api/requirements.txt
+WORKDIR /var/app/api
+RUN pip install --no-cache-dir --upgrade -r /var/app/api/requirements.txt
+COPY api /var/app/api
 CMD ["fastapi", "run", "main.py", "--port", "80"]
--- api/main.py
+++ api/main.py
@@ -1,24 +1,31 @@
 from typing import List
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
-import database
 import schemas
 import models
-from database import db_state_default
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="../ui/build/static", check_dir=False), name="static")
+
+
+@app.get("/")
+def serve_react_app():
+    return FileResponse("../ui/build/index.html")
+
 
 @app.get("/movies", response_model=List[schemas.Movie])
 def get_movies():
     return list(models.Movie.select())
-    # movies = crud.get_movies()
-    # return movies
+
 
 @app.post("/movies", response_model=schemas.Movie)
 def add_movie(movie: schemas.MovieBase):
     movie = models.Movie.create(**movie.dict())
     return movie
+
 
 @app.get("/movies/{movie_id}", response_model=schemas.Movie)
 def get_movie(movie_id: int):
@@ -26,6 +33,7 @@ def get_movie(movie_id: int):
     if db_movie is None:
         raise HTTPException(status_code=404, detail="Movie not found")
     return db_movie
+
 
 @app.delete("/movies/{movie_id}", response_model=schemas.Movie)
 def get_movie(movie_id: int):
