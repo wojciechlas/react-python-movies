53fddf2ef5b8db0a28d37d566070c3bdbef7985c
Wojciech Łaś <w.las@live.com>
2025-01-25 09:49:13 +0100

    Pobieranie filmów

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import "milligram";
 import MovieForm from "./MovieForm";
 import MoviesList from "./MoviesList";
@@ -7,6 +7,17 @@ import MoviesList from "./MoviesList";
 function App() {
     const [movies, setMovies] = useState([]);
     const [addingMovie, setAddingMovie] = useState(false);
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            const response = await fetch('/movies');
+            if (response.ok) {
+                const movies = await response.json();
+                setMovies(movies);
+            }
+        };
+        fetchMovies();
+    }, []);
 
     async function handleAddMovie(movie) {
         const response = await fetch('/movies', {
